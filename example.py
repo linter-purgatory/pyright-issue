3ce4ca4384af25be9534f970538ef3d67954ded9
Jayden Milne <jaydenm@qualtrics.com>
2023-03-08 17:03:42 -0700

    Turn on strict checking mode

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 # pyright: basic,reportUnknownLambdaType=warning
 
-def fn(unknown_variable_type): # this should be surpressed in basic mode
+def fn(unknown_variable_type): # this should be surpressed in basic mode, but isn't
     print(unknown_variable_type)
 
     lambda y: 1 # this error is correctly turned into a warning via the comment
